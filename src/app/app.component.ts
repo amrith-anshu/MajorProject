69fae019a4d36ba61aa645171bf318217f22feeb
Amrithanshu Kesoju <92728684+amrith-anshu@users.noreply.github.com>
2023-04-11 14:45:59 +0530

    sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+//import { SafeUrl } from '@angular/platform-browser';
+import { MatSidenav } from '@angular/material/sidenav';
+import { DomSanitizer } from '@angular/platform-browser';
+import { QRCodeModule } from 'angularx-qrcode';
+import { QRCode } from 'qrcode';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) { }
+
+
+ selectedColor: string = '#007bff';
   title = 'image';
+  imageData: any;
+  file: any;
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  arrayColors: any = {
+    color1: '#2883e9',
+    color2: '#e920e9',
+    color3: 'rgb(255,245,0)',
+    color4: 'rgb(236,64,64)',
+    color5: 'rgba(45,208,45,1)'
+  };
+  imagePreview: any =
+    'https://angular.io/assets/images/logos/angular/angular.png';
+  qrData = 'https://trst01.com/';
+  // imageUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+  chooseFile(event: any) {
+    this.file = event.target.files[0];
+    console.log(this.file);
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.imagePreview = reader.result;
+      };
+      reader.readAsDataURL(file);
+    }
+ }
+
+onColorChange(value: string) {
+  this.selectedColor = value;
+}
+
+downloadImage() {
+  this.http.get('qrcode', { responseType: 'blob' })
+    .subscribe((blob: Blob) => {
+      const objectUrl = URL.createObjectURL(blob);
+      this.imageData = this.sanitizer.bypassSecurityTrustUrl(objectUrl);
+      const downloadLink = document.createElement('a');
+      downloadLink.href = objectUrl;
+      downloadLink.download = 'myimage.png';
+      downloadLink.click();
+      window.URL.revokeObjectURL(objectUrl);
+      downloadLink.remove();
+    });
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QRCodeModule } from 'angularx-qrcode';
+import { HttpClientModule} from '@angular/common/http';
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ColorPickerModule } from 'ngx-color-picker';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QRCodeModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
+    MatListModule,
+    ColorPickerModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
-export class AppModule { }
+export class AppModule {}
